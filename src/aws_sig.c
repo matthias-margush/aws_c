86dc653660de7b9620980a6fe7e6265d0109c242
Matthias Margush <matthias.margush@gmail.com>
2018-02-26 19:40:29 -0800

    wip

--- src/aws_sig.c
+++ src/aws_sig.c
@@ -1,10 +1,12 @@
 #include "aws_creds.h"
+#include <dish.h>
 #include <openssl/hmac.h>
 #include <string.h>
 #include <time.h>
 
-char *v4_key(const char *const secret_access_key) {
-	char *key = malloc(strlen("AWS4") + strlen(secret_access_key));
+char *v4_key(const char *const secret_access_key, struct Dish *dish) {
+	//char *key = malloc(strlen("AWS4") + strlen(secret_access_key));
+	char *key = dish_stacked(dishwasher, strlen("AWS4") + strlen(secret_access_key));
 	if (!key) {
 		return NULL;
 	}
@@ -29,8 +31,12 @@ unsigned char *hmac(const char *const key, const char *const data,
 unsigned char *aws_sig_v4(struct AwsCreds *creds,
                           const char *const aws_region,
                           const char *const aws_service,
-                          const char *const request, unsigned char *md,
+						  const char *const request, unsigned char *md,
                           unsigned int *md_len) {
+	dish_init();
+	struct Dish dish;
+	dish_create(&dish);
+
 	char *key = v4_key(aws_creds_secret_access_key(creds));
 	if (!key) {
 		return NULL;
@@ -38,6 +44,10 @@ unsigned char *aws_sig_v4(struct AwsCreds *creds,
 	char date[9];
 	now_yyyymmdd(&date);
 	unsigned char *ret = hmac(key, date, md, md_len);
-	free(key);
+	//free(key);
+	
+	dish_reset(&dish);
+
+	dish_destroy(&dish);
 	return ret;
 }
